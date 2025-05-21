e8fe2116a6bd0f8e76aa6daa6dbed1a39813f4c5
stevehaworth02 <0218steven@gmail.com>
2025-05-20 20:20:53 -0500

    Using column transform

--- projects/model_sklearn.py
+++ projects/model_sklearn.py
@@ -6,12 +6,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import Normalizer
+
 # === 2. Load & prepare the data ===
 df = pd.read_csv("data/Housing.csv")
 print(f"Columns: {df.columns}\n Rows: {len(df)}") 
+numerical_cols = df.select_dtypes(include=['int64', 'float64']).columns
+categorical_Cols = df.select_dtypes(exclude=['int64', 'float64']).columns
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", Normalizer(), numerical_cols),
+        ('cat', 'passthrough', categorical_Cols)
+    ]
+)
+
+
 X = df[["area", "bathrooms"]].to_numpy()
 y = df["price"].to_numpy()
-X = preprocessing.normalize(X)
-y = preprocessing.normalize(y)
 print(f"Feature Matrix Dimensions: {X.shape}\n Feature Matrix X: {X[0:5]}")
 # === 3. Train/Test Split ===
