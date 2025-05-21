bdc40d735b57b6e1caeb9235a31e544a0a5ad027
stevehaworth02 <0218steven@gmail.com>
2025-05-20 20:24:16 -0500

    Bug fix

--- projects/model_sklearn.py
+++ projects/model_sklearn.py
@@ -14,14 +14,15 @@ df = pd.read_csv("data/Housing.csv")
 print(f"Columns: {df.columns}\n Rows: {len(df)}") 
 numerical_cols = df.select_dtypes(include=['int64', 'float64']).columns
 categorical_Cols = df.select_dtypes(exclude=['int64', 'float64']).columns
+columns = numerical_cols + categorical_Cols
 preprocessor = ColumnTransformer(
     transformers=[
         ("num", Normalizer(), numerical_cols),
         ('cat', 'passthrough', categorical_Cols)
     ]
 )
-
-
+df = preprocessor.fit_transform(df)
+df = pd.DataFrame(df, columns=columns)
 X = df[["area", "bathrooms"]].to_numpy()
 y = df["price"].to_numpy()
 print(f"Feature Matrix Dimensions: {X.shape}\n Feature Matrix X: {X[0:5]}")
